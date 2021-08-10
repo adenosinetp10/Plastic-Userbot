ef149821309cef2890eab7315ce97caded5b09fe
Plastic Cat <63637385+notPlasticCat@users.noreply.github.com>
2021-08-11 01:33:22 +0530

    Create example_config.py

---
+++ plastic/example_config.py
@@ -0,0 +1,8 @@
+class Config(object):
+    api_id: int = 1234567
+    api_hash: str = "1234567abcdegh177013ndhferis"
+    
+    COMMAND: list = "? . _ !".split()
+
+    USERBOT_VER: str = "0.0.1"
+    REPO: str = "https://linktoyouruserbotrepo.com"
